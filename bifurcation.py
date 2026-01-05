3ffa091997768536b225af7c18ee00dc08a9db3f
Eduardo <ehallabreu@gmail.com>
2026-01-05 12:27:02 -0800

    improving the graph

--- bifurcation.py
+++ bifurcation.py
@@ -14,15 +14,17 @@ def bifurcation(n, x0, r_vec, k):
         x[i] = logistic_map(n, x0, r_vec[i])[k:]
     return x
 
-step = 0.0001
+step = 0.0005
 r_vec = np.arange(2.4, 4+step, step)
-xn = bifurcation(n=2000, x0=0.2, r_vec=r_vec, k=1000)
+xn = bifurcation(n=2000, x0=0.2, r_vec=r_vec, k=500)
 
 R = np.repeat(r_vec, xn.shape[1])
 X = xn.flatten()
 
+plt.figure(figsize=(12, 7))
 plt.scatter(R, X, color='black', s=0.001, alpha=0.1)
+plt.title('Logistic map bifurcation diagram')
 plt.xlabel('r')
 plt.ylabel('x')
-plt.xticks(np.arange(2.4, 4.2, 0.2))
+plt.xticks(np.arange(2.8, 3.6, 0.2))
 plt.show()
