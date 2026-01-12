b03b0818b4b3e8b4f964efb47aa86c50a3b0e931
Eduardo <ehallabreu@gmail.com>
2026-01-11 18:44:35 -0800

    implementing the trapezoidal method

--- euler_methods.py
+++ euler_methods.py
@@ -21,7 +21,7 @@ def backward_euler(x0, v0, length, dt, k, m, g):
     forward = np.empty([length, 2]) 
     forward[0] = np.array([x0, v0]) 
     
-    backward = np.empty([length, 2])
+    backward = np.empty_like(forward)
     backward[0] = np.array([x0, v0]) 
 
     for n in range(1, length):
@@ -30,40 +30,32 @@ def backward_euler(x0, v0, length, dt, k, m, g):
             backward[n-1, 1] + dt * ((-k/m) * forward[n-1, 0] + g) # = vFE[n]
         ])
         
-        for n in range(1, length):
-            backward[n] = np.array([
-                backward[n-1, 0] + dt * forward[n, 1],               # = x[n]
-                backward[n-1, 1] + dt * ((-k/m) * forward[n, 0] + g) # = v[n]
-            ])
+        backward[n] = np.array([
+            backward[n-1, 0] + dt * forward[n, 1],               # = x[n]
+            backward[n-1, 1] + dt * ((-k/m) * forward[n, 0] + g) # = v[n]
+        ])
 
     return backward
 
 def trapezoidal(x0, v0, length, dt, k, m, g):
+
     forward = np.empty([length, 2]) 
     forward[0] = np.array([x0, v0]) 
-    
-    backward = np.empty_like(forward)
-    backward[0] = np.array([x0, v0]) 
 
     average = np.empty_like(forward)
     average[0] = np.array([x0, v0]) 
 
-
     for n in range(1, length):
         forward[n] = np.array([
-            average[n-1, 0] + dt * forward[n-1, 1],   # = xFE[n]
+            average[n-1, 0] + dt * forward[n-1, 1],               # = xFE[n]
             average[n-1, 1] + dt * ((-k/m) * forward[n-1, 0] + g) # = vFE[n]
         ])
         
-        for n in range(1, length):
-            backward[n] = np.array([
-                backward[n-1, 0] + dt * forward[n, 1],  # = x[n]
-                backward[n-1, 1] + dt * ((-k/m) * forward[n, 0] + g) # = v[n]
-            ])
-
-            for n in range(1, length):
-                average[n] = average[n-1] + ((forward[n-1] + backward[n-1]) / 2)
-
+        average[n] = np.array([
+            average[n-1, 0] + dt/2 * (forward[n, 1] + forward[n-1, 1]),               
+            average[n-1, 1] + dt/2 * (((-k/m) * forward[n, 0] + g) + ((-k/m) * forward[n-1, 0] + g))
+        ])
+                    
     return average
     
 # trying the implicit way
@@ -85,18 +77,19 @@ def implicit_euler(x0, v0, length, dt, k, m, g):
 
     return trajectory
 
-trajectory = forward_euler(x0=-1, v0=-2, length=500, dt=0.1, k=2, m=0.5, g=0)
+trajectory = forward_euler(x0=-1, v0=-2, length=50, dt=0.1, k=2, m=0.5, g=0)
 print(trajectory[:6])
 
-trajectory_backward = backward_euler(x0=-1, v0=-2, length=500, dt=0.1, k=2, m=0.5, g=0)
+trajectory_backward = backward_euler(x0=-1, v0=-2, length=50, dt=0.1, k=2, m=0.5, g=0)
 print(trajectory_backward[:6])
 
-trapezoid = trapezoidal(x0=-1, v0=-2, length=500, dt=0.1, k=2, m=0.5, g=0)
+trapezoid = trapezoidal(x0=-1, v0=-2, length=500, dt=0.01, k=2, m=0.5, g=0)
+print(trapezoid[:6])
 
-# plt.plot(trajectory_backward[:,0], trajectory_backward[:,1], color='blue', label='backward')
-# plt.plot(trajectory[:,0], trajectory[:,1], color='red', label='forward')
+plt.plot(trajectory_backward[:,0], trajectory_backward[:,1], color='blue', label='backward')
+plt.plot(trajectory[:,0], trajectory[:,1], color='red', label='forward')
 plt.plot(trapezoid[:,0], trapezoid[:,1], color='green', label='trapezoid')
-plt.title('harmonic oscilator euler solvers')
+plt.title('SHO euler solvers')
 plt.legend()
 plt.xlabel('x')
 plt.ylabel('v')
